51908fc0fffb85de44cd226b6d3d52c563a018d4
teryakiii <splendidzero@hotmail.com>
2018-12-30 23:41:52 +0800

    12-31 layout function fixed

--- fsviewer/dump_layout.c
+++ fsviewer/dump_layout.c
@@ -8,32 +8,30 @@
 #include <string.h>
 
 #define BASE_OFFSET 1024                   /* locates beginning of the super block (first group) */
-#define FD_DEVICE "/dev/loop0"               /* the floppy disk device */
+//#define FD_DEVICE "/dev/loop0"               /* the floppy disk device */
 
 static unsigned int block_size = 0;        /* block size (to be calculated) */
 
-void dump_layout(char *device_name)
+//
+int isPower(int n)
 {
-//  FILE *fp = fopen(device_name, "rb");
-//  // disclaimer: i don't know the exact layout of ext2 file system, please refer to the documentation or book
-//  // suppose the superblock is located at the first 1024 bytes - 2048 bytes (i assume block size is 1024, which may not
-//  // be the case! google for how to get block size!)
-//
-//  fseek(fp, 1024, SEEK_SET); // offset the file pointer to the right address
-//  char buffer[1024];
-//  if(fread(buffer, 1, 1024, fp) != 1024)
-//    fprintf(stderr, "device file too small, cannot read super block");
-//
-//  // now parse the binary data
-//  // TODO: this might not be the correct struct to cast to, refer to documentation!
-//  struct super_block *super_block_data = (struct super_block *) buffer; // force cast the binary char array to struct super_block
-//  // now read from super_block_data, etc.
-    struct ext2_super_block super;
-    struct ext2_group_desc group;
+    int i;
+    int a[12] = { 0, 1, 3, 5, 7, 9, 25, 27, 49, 81, 125, 243 };
+    for (i = 0; i < 12; i++)
+        if (n == a[i])
+            return 1;
+    return 0;
+}
 
+int main(int argc, char * argv[])
+{
+    char * FD_DEVICE = argv[1];
+
+    struct ext2_super_block super;
     int fd;
 
-    /* open floppy device */
+
+    /* read super-block */
 
     if ((fd = open(FD_DEVICE, O_RDONLY)) < 0) {
         perror(FD_DEVICE);
@@ -44,63 +42,88 @@ void dump_layout(char *device_name)
 
     lseek(fd, BASE_OFFSET, SEEK_SET);
     read(fd, &super, sizeof(super));
+    close(fd);
 
+    // is ext2 func
     if (super.s_magic != EXT2_SUPER_MAGIC) {
         fprintf(stderr, "Not a Ext2 filesystem\n");
         exit(1);
     }
+    block_size = 1024 << super.s_log_block_size;
 
-    block_size =1024 << super.s_log_block_size;
+    /*
+    ext2_super_block
+    super.s_blocks_per_group       块组大小（块）
+    super.s_reserved_gdt_blocks    reserved_GDT大小
+    super.s_inodes_per_group       每组节点数
+    super.s_inode_size             节点大小（字节）
+    */
 
-    /* read group descriptor */
+    int group_num = (super.s_blocks_count - 1) / super.s_blocks_per_group + 1;
+    struct ext2_inode inode;
+    int i, j, a, b, delete_num = 0, check_num = 0;
+    int mark = 1;  //访问标记
 
-    lseek(fd, BASE_OFFSET + block_size, SEEK_SET);
-    read(fd, &group, sizeof(group));
-    close(fd);
+    // 每个块组中 inode table 所占的块数
+    int block_inodes_per_group = super.s_inodes_per_group*super.s_inode_size / block_size;
 
-    /* number of inodes per block */
-    unsigned int inodes_per_block = block_size / sizeof(struct ext2_inode);
+    // GDT所占的块数
+    struct ext2_group_desc group_desc;
+    int GDT_size = sizeof(group_desc)*group_num / block_size + 1;
 
-    /* size in blocks of the inode table */
-    unsigned int itable_blocks = super.s_inodes_per_group / inodes_per_block;
+    printf("\n dump_layout: \n\n");
 
-    /* calculate number of block groups on the disk */
-    unsigned int group_count = 1 + (super.s_blocks_count-1) / super.s_blocks_per_group;
+    // 依次访问每个块组
+    char *start = "start";
+    char *end = "end";
+    char *lenth = "lenth";
+    char *placeholder = "------";
+    char *table_column[] = {"Group", "Superblock", "GDT", "Reserved GDT",
+                            "Data block bitmap", "Inode bitmap", "Inode table", "Data block"};
+    for (i = 0; i < group_num; i++)
+    {
+        mark = 1 + i * super.s_blocks_per_group;
+        if (isPower(i) == 1)
+        {
+            printf("%-5s %-11d:|%6s|%6s|%6s|\n", table_column[0],i, start, end, lenth);
+            a = mark, b = mark + 1 - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[1], a, b, 1);
+            a = b + 1, b = a + GDT_size - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[2], a, b, GDT_size);
+            a = b + 1, b = a + super.s_reserved_gdt_blocks - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[3], a, b, super.s_reserved_gdt_blocks);
+            a = b + 1, b = a + 1 - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[4], a, b, 1);
+            a = b + 1, b = a + 1 - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[5], a, b, 1);
+            a = b + 1, b = a + block_inodes_per_group - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[6], a, b, block_inodes_per_group);
+            a = b + 1, b = mark + super.s_blocks_per_group - 1;
+            if (b > super.s_blocks_count - 1)
+                b = super.s_blocks_count - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[7], a, b, b - a + 1);
+        }
+        else
+        {
 
-    /* calculate size of the group descriptor list in bytes */
-    unsigned int descr_list_size = group_count * sizeof(group);
-    printf("superblock:%u\n"
-           "GDT:%u-%u\n"
-           "reserved GDT:%u-%u\n"
-           "blocks bitmap block: %u\n"
-           "inodes bitmap block: %u\n"
-           "inodes table block : %u-%u\n"
-           "data block:%u-%u\n"
-            ,
-           super.s_first_data_block,
-           super.s_first_data_block + 1, group.bg_block_bitmap - super.s_reserved_gdt_blocks - 1 ,//1 has something wrong
-           group.bg_block_bitmap - super.s_reserved_gdt_blocks, group.bg_block_bitmap - 1,
-           group.bg_block_bitmap,
-           group.bg_inode_bitmap,
-           group.bg_inode_table, group.bg_inode_table + itable_blocks - 1,
-           group.bg_inode_table + itable_blocks, block_size - 1
-    );
-    
-}
+            printf("%-5s %-11d:|%6s|%6s|%6s|\n", table_column[0], i, start, end, lenth);
+            printf("%-17s:|%6s|%6s|%6s|\n", table_column[1], placeholder,placeholder,placeholder);
+            printf("%-17s:|%6s|%6s|%6s|\n", table_column[2], placeholder,placeholder,placeholder);
+            printf("%-17s:|%6s|%6s|%6s|\n", table_column[3], placeholder,placeholder,placeholder);
 
+            a = mark, b = mark + 1 - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[4], a, b, 1);
+            a = b + 1, b = a + 1 - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[5], a, b, 1);
+            a = b + 1, b = a + block_inodes_per_group - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[6], a, b, block_inodes_per_group);
+            a = b + 1, b = mark + super.s_blocks_per_group - 1;
+            if (b > super.s_blocks_count - 1)
+                b = super.s_blocks_count - 1;
+            printf("%-17s:|%6d|%6d|%6d|\n", table_column[7], a, b, b - a + 1);
+        }
+        printf("\n");
+    }
 
-int main(int argc, char **argv)
-{
-  // TODO: need MUCH cleaner argument parser
-  // this is just an example of concept
-  // consider getopt or argp
-  // see https://www.gnu.org/software/libc/manual/html_node/Getopt.html
-  // and https://www.gnu.org/software/libc/manual/html_node/Argp.html
-  // and https://stackoverflow.com/questions/9642732/parsing-command-line-arguments
-  if(argc < 2)
-    printf("Need device file argument");
-
-  dump_layout(argv[1]);
-
-  return 0;
+    exit(0);
 }
